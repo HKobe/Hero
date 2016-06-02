3889b7d0ca7cb97a6ebdcfe83245b19a58532713
HKobe <HKobeissi1@hotmail.com>
2016-06-02 10:03:13 -0400

    Added BeginFight(), Documentation, and cleaned up code.

--- Hero/Hero.cs
+++ Hero/Hero.cs
@@ -3,28 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * @Author Name & StudentNumber: Hassan Kobeissi- 300847239
+ * @Last Modified: June 3rd, 2016.
+ * @Program Description: Create a Hero. 
+ */
 namespace Hero
 {
+    /// <summary>
+    /// This is the Hero Class
+    /// </summary>
     class Hero
     {
+        static Random random = new Random();
         #region Private Instance Variables
-        /*Hero's attributes*/
+       /// <summary>
+       /// Private instance variable. Hero's Attributes.
+       /// </summary>
         private int _strength;
         private int _speed;
         private int _health;
         private string _name;
         #endregion
 
-        #region Public Methods
-        /*
-         * <summary>
-         * This methods enables the Strength attribute for the Hero class 
-         * </summary>
-         * @method: Strength
-         * @return: {int}
-         */
-        public int Strength
+        #region Private Methods (get;set;)
+
+
+        /// <summary>
+        /// This enables the Strength attributes for the hero class
+        /// </summary>
+        /// @method: Strength
+        /// @return: {int}    
+        private int Strength
         {
             get
             {
@@ -36,47 +46,56 @@ namespace Hero
             }
 
         }
-        private int Speed
-        {
-            get
-            {
-                return _speed;
-            }
-            set
-            {
-                this._speed = value;
-            }
-        }
-        private int Health
-        {
-            get
-            {
-                return _health;
-            }
-            set
-            {
-                this._health = value;
-            }
-        }
-        private string Name
-        {
-            get
-            {
-                return _name;
-            }
-            set
-            {
-                this._name = value;
-            }
-        }
+         private int Speed
+         {
+             get
+             {
+                 return _speed;
+             }
+             set
+             {
+                 this._speed = value;
+             }
+         }
+         private int Health
+         {
+             get
+             {
+                 return _health;
+             }
+             set
+             {
+                 this._health = value;
+             }
+         }
+         private string Name
+         {
+             get
+             {
+                 return _name;
+             }
+             set
+             {
+                 this._name = value;
+             }
+         }
         #endregion
 
-        public Hero(string name)
+        #region Public Hero(string name) , _generateAbilities();
+        /// <summary>
+        /// Constructor Method, calls generate abilities and passes name value.
+        /// </summary>
+        /// <param name="_name">Hero name</param>
+        public Hero(string _name)
         {
             _generateAbilities();
-            this._name = name;
+            this.Name = _name;
         }
 
+
+        /// <summary>
+        /// Generates random ability numbers..
+        /// </summary>
         private void _generateAbilities()
         {
             Random random = new Random();
@@ -84,26 +103,31 @@ namespace Hero
             this._health = random.Next(1, 100);
             this._speed = random.Next(1, 100);
         }
+        #endregion
 
-        #region Public Methods
-        /*
-         * <summary>
-         * Public Fight Methods displays the private method _hitAttempt()
-         * </summary>
-         * @returns: {void}
-         */
+        #region Public void Fight()
+        /// <summary>
+        /// Public Fight Method displays the attacks hit/missed 
+        /// </summary>
+        /// @returns: {void}
         public void Fight()
         {
             if (_hitAttempt() == true)
             {
                 Console.WriteLine(_hitDamage());
-                                        
             }
-           
+            else
+            {
+                Console.WriteLine("Missed!");
+            }
+
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private int _hitDamage()
         {
-            Random random = new Random();
             int damage = random.Next(1, 6);
             damage = damage * _strength;
             return damage;
@@ -111,7 +135,6 @@ namespace Hero
         private bool _hitAttempt()
         {
             int hit;
-            Random random = new Random();
             hit = random.Next(1, 5);
             if (hit == 1)
             {
@@ -122,14 +145,17 @@ namespace Hero
                 return false;
             }
         }
+        #endregion
+
+        #region public void Show()
+        /// <summary>
+        /// Public Method that displays Hero's stats.
+        /// </summary>
+        /// @Returns: {void}
+        /// @Params: None
         public void Show()
         {
-            /*<summary>
-            Public method that displays Hero Stats 
-            </summary>
-            @Returns: {void}
-             */
-            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(" Fighting Ability Chart");
             Console.WriteLine(" ----------------------");
             Console.WriteLine(" HP       STR     SPEED");
@@ -140,6 +166,17 @@ namespace Hero
         }
         #endregion
 
+        #region public void BeginFight() 
+        /// <summary>
+        /// Public method, Displays BeginFight prompt.
+        /// </summary>
+        /// @Returns: Void
+        /// @Params: None
+        public void BeginFight()
+        {
+            Console.WriteLine(this._name + " Enters, Begin Fight!");
+        }
+        #endregion
     }
-    
+
 }
--- Hero/Program.cs
+++ Hero/Program.cs
@@ -5,22 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 /*
  * <summary>
- * This is my driver class.
+ * This is the driver class.
  * </summary>
  * @Author Name & StudentNumber: Hassan Kobeissi- 300847239
  * @Last Modified: June 3rd, 2016.
- * @Program Description: Create a Hero, 
+ * @Program Description: Create a Hero 
  */
 namespace Hero
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            /*Create new instance of Hero*/
             Hero Kobe = new Hero("Kobe");
+
+            /*Display Hero Statistics*/
             Kobe.Show();
             Console.WriteLine();
+
+            /*Fight Prompt*/
+            Kobe.BeginFight();
+
+            Console.WriteLine();
+
+            /*Attacks*/
+            for (int i = 0; i < 20 ; i++)
+            {
             Kobe.Fight();
+            }
+            
             
             
             
